b0c04f5e2b72feb073e5b30fba34e7dd080da9d1
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-05 22:04:53 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -4,6 +4,6 @@ end
 
 #using the current year instead of a fixed year
 
-def current_age_for_birth_year(birth_year)
-  Time.now.year - birth_year
-end
+#def current_age_for_birth_year(birth_year)
+#  Time.now.year - birth_year
+#end
